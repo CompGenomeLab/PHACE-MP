eadca046471f479bc4484590c7ad5239e51d6251
Mustafa Malkoç <129995816+mustafamalkoc@users.noreply.github.com>
2026-04-22 23:08:06 +0300

    Implement masking for artificial gaps and load metadata
    Added functionality for masking artificial gaps and loading metadata for protein boundaries and ortholog tables.

--- PHACE_Codes/MSA2.R
+++ PHACE_Codes/MSA2.R
@@ -9,18 +9,41 @@ args = commandArgs(trailingOnly=TRUE)
 
 id <- args[1]
 
-file_fasta <- sprintf("%s_MaskedMSA.fasta", id)
+file_fasta <- args[2]
 
 # Read fasta file, MSA
 fasta <- read.fasta(file = file_fasta)
 msa <- fasta$ali
 
+# --- MASKING PATCH START ---
+# Load metadata (MANDATORY)
+boundaries_path <- args[3]
+ortholog_table_path <- args[4]
+
+source("./PHACE_Codes/load_metadata.R")
+protein_boundaries <- load_protein_boundaries(boundaries_path)
+ortholog_matrix <- load_ortholog_table(ortholog_table_path, rownames(msa))
+cat("Metadata loaded. Artificial gaps will be encoded as missing data.\n")
+# --- MASKING PATCH END ---
+
 for (i in 1:length(msa[1,])){
+  # --- MASKING PATCH START ---
+  # Determine which leaves are missing this protein
+  mask_leaves <- get_masked_leaves_for_position(i, protein_boundaries, ortholog_matrix)
+  # --- MASKING PATCH END ---
+  
   gap <- which(msa[,i]=="-")
   oth <- which(msa[,i]!="-")
   
   msa[gap,i] <- "G"
   msa[oth,i] <- "C"
+  
+  # --- MASKING PATCH START ---
+  # Overwrite artificial gaps with missing data
+  if (length(mask_leaves) > 0) {
+    msa[mask_leaves, i] <- "?"
+  }
+  # --- MASKING PATCH END ---
 }
 
 num_leaves <- length(msa[,1])
