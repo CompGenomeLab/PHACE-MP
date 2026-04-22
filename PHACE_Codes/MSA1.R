5f900a21f4ddd447d8fe798df8ecfd774c21b0f9
Mustafa Malkoç <129995816+mustafamalkoc@users.noreply.github.com>
2026-04-22 23:06:11 +0300

    Implement masking for artificial gaps and load metadata
    Added functionality for masking artificial gaps and loading metadata for protein boundaries and ortholog tables.

--- PHACE_Codes/MSA1.R
+++ PHACE_Codes/MSA1.R
@@ -12,19 +12,36 @@ res <- read.csv(sprintf("ToleranceScores/%s.csv", id))
 eps <- 10^(-15)
 res[,2:21] <- 1-log(res[,2:21] +eps)/log(eps)
 
-file_fasta <- sprintf("%s_MaskedMSA.fasta", id)
+file_fasta <- args[2]
 
 # Read fasta file, MSA
 fasta <- read.fasta(file = file_fasta)
 msa <- fasta$ali
 
+# --- MASKING PATCH START ---
+# Load metadata (MANDATORY)
+boundaries_path <- args[3]
+ortholog_table_path <- args[4]
+
+source("./PHACE_Codes/load_metadata.R")
+protein_boundaries <- load_protein_boundaries(boundaries_path)
+ortholog_matrix <- load_ortholog_table(ortholog_table_path, rownames(msa))
+cat("Metadata loaded. Artificial gaps will be encoded as missing data.\n")
+# --- MASKING PATCH END ---
+
 for (i in 1:length(msa[1,])){
   sc <- res[i, 2:21]
-  obs <- table(msa[which(msa[,i]!="-"),i])
+  
+  # --- MASKING PATCH START ---
+  # Exclude both "-" AND "?" from dominant state computation
+  valid_rows <- which(msa[,i] != "-" & msa[,i] != "?")
+  obs <- table(msa[valid_rows, i])
+  # --- MASKING PATCH END ---
+  
   dom <- names(sort(obs, decreasing = T))[1]
   
   lc <- which(colnames(sc)==dom)
-  dom_p2 <- names(sc)[which(as.numeric(sc)>as.numeric(sc[lc]))]
+  dom_p2 <- names(sc)[which(as.numeric(sc)>=as.numeric(sc[lc]))]
   
   dom_loc <- c()
   for (aa in c(dom, dom_p2)){
@@ -36,6 +53,14 @@ for (i in 1:length(msa[1,])){
   upd <- intersect(which(msa[,i]!="XX"), which(msa[,i]!="-"))
   msa[upd,i] <- "A"
   msa[dom_loc, i] <- "C"
+  
+  # --- MASKING PATCH START ---
+  # Overwrite artificial gaps with missing data AFTER C/A/- encoding
+  mask_leaves <- get_masked_leaves_for_position(i, protein_boundaries, ortholog_matrix)
+  if (length(mask_leaves) > 0) {
+    msa[mask_leaves, i] <- "?"
+  }
+  # --- MASKING PATCH END ---
 }
 
 num_leaves <- length(msa[,1])
